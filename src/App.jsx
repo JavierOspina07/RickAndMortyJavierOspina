4fb08e205951b482e3486c683593f9a0b703b0d5
Javier Ospina <jaheosig10@gmail.com>
2023-07-07 00:23:13 -0500

    basico de rick

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import './App.css'
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import getRandomNumber from "./utils/getRandomNumber";
+import LocationInfo from "./components/LocationInfo";
+import ResidentCard from "./components/ResidentCard";
 
 function App() {
+  const [location, setLocation] = useState();
+
+  useEffect(() => {
+    const url = `https://rickandmortyapi.com/api/location/${getRandomNumber(
+      126
+    )}`;
+    axios
+      .get(url)
+      .then((res) => setLocation(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <h1>Rick and Morty</h1>
+      <LocationInfo location={location} />
+      <div>
+        {
+          location?.residents.map(url =>(
+            <ResidentCard
+              key={url}
+              url={url}
+            />
+          ))
+        }
+      </div>
+    </div>
+
+
+    
+  );
 }
 
-export default App
+export default App;
